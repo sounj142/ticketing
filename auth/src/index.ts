f9391cb8cb7d9a76b7867b1fa3827ae57871939f
Hoang <sounj142@gmail.com>
2022-01-12 22:38:28 +0700

    end of section 5

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
